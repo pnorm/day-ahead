8deb4102e11fc90ba9a803936606ec605296b8a9
Pawel <pawel.normann@gmail.com>
2023-11-27 03:51:18 +0100

    Initial structure of the code.

---
+++ create_project_structure.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Directory structure
+directories=(
+    "api"
+    "models"
+    "notebooks"
+    "references"
+    "reports/figures"
+    "src/data/logs"
+    "src/data/tests"
+    "src/data/utils"
+    "src/features"
+    "src/models"
+    "src/visualization"
+)
+
+# Create dirs
+for dir in "${directories[@]}"; do
+    mkdir -p "$dir"
+done
+
+echo "Directory structure created successfully!"
+
+src_directories=(
+    "src"
+    "src/data"
+    "src/features"
+    "src/models"
+    "src/visualization"
+)
+
+# Create __init__.py files in the specified directories
+for dir in "${src_directories[@]}"; do
+    touch "$dir/__init__.py"
+done
+
+echo "__init__.py files created successfully in src/ and its subdirectories!"
+
+unzip data.zip
+
+echo "data.zip extracted successfully."
+
+touch README.md
+touch .gitignore
+touch env.sample
+
+echo "README.md .gitignore env.sample files created."
+
