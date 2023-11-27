65fec8ab51e1520f21d5e4c05da2398971cf8f31
Pawel <pawel.normann@gmail.com>
2023-11-27 21:34:38 +0100

    Utilities for creating list of dates created, test added accordingly.

--- src/data/tests/test_date_utility.py
+++ src/data/tests/test_date_utility.py
@@ -1,9 +1,11 @@
-from datetime import date
+from datetime import date, timedelta
 import pytest
 
 from ..utils.date_utility import (
     validate_date_format,
-    validate_date_range
+    validate_date_range,
+    generate_date_list,
+    generate_date_list_n_days_from_today
 )
 
 
@@ -30,3 +32,26 @@ def test_validate_date_range_invalid():
     to_date = "2023-11-30"
     with pytest.raises(ValueError):
         validate_date_range(from_date, to_date)
+
+
+def test_generate_date_list_valid_range():
+    from_date_str = date(2023, 1, 1)
+    to_date_str = date(2023, 1, 5)
+    result = generate_date_list(from_date_str, to_date_str)
+    expected_result = ['2023-01-01', '2023-01-02', '2023-01-03', '2023-01-04', '2023-01-05']
+    assert result == expected_result
+
+
+def test_generate_date_list_single_day():
+    from_date_str = date(2023, 2, 10)
+    to_date_str = date(2023, 2, 10)
+    result = generate_date_list(from_date_str, to_date_str)
+    expected_result = ['2023-02-10']
+    assert result == expected_result
+
+
+def test_generate_date_list_invalid_range():
+    from_date_str = date(2023, 3, 1)
+    to_date_str = date(2023, 2, 28)
+    with pytest.raises(ValueError):
+        generate_date_list(from_date_str, to_date_str)
--- src/data/utils/date_utility.py
+++ src/data/utils/date_utility.py
@@ -1,5 +1,5 @@
 from datetime import date, datetime, timedelta
-from typing import Optional
+from typing import Optional, List
 
 
 def validate_date_format(s: str) -> Optional[date]:
@@ -11,6 +11,7 @@ def validate_date_format(s: str) -> Optional[date]:
     except ValueError:
         raise ValueError("Invalid date format. Please use YYYY-MM-DD.")
     
+    
 def validate_date_range(from_date: date, to_date: date):
     """
         Check if from_date is before to_date.
@@ -18,3 +19,29 @@ def validate_date_range(from_date: date, to_date: date):
     if from_date > to_date:
         raise ValueError("Invalid date range. 'from_date' must be before or equal to 'to_date'.")
     
+
+def generate_date_list(from_date: date, to_date: date) -> List[str]:
+    """
+        Generate list of dates in a given range.
+    """
+    validate_date_range(from_date, to_date)
+
+    date_list = []
+    current_date = from_date
+    while current_date <= to_date:
+        date_list.append(current_date.strftime('%Y-%m-%d'))
+        current_date += timedelta(days=1)
+
+    return date_list
+
+
+def generate_date_list_n_days_from_today(day_lag: int = 60) -> List[str]:
+    """
+        Generate list of dates, 60 dates back from today.
+    """
+    today = datetime.today()
+    from_date = datetime.today() - timedelta(days=day_lag)
+
+    date_list = generate_date_list(from_date=from_date, to_date=today)
+
+    return date_list
